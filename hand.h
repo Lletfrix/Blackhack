edd5b262d1df3b431ddd757624b0b436e0b10bd9
lletfrix <lletfrix@gmail.com>
2017-03-25 16:11:40 +0100

    hand_value specification

--- hand.h
+++ hand.h
@@ -14,7 +14,11 @@ void hand_destroy(Hand*);
 /*  insert a card on the hand, return NULL if error */
 Hand* hand_insertCard(Hand*, int rank);
 
-/* get the value of the Hand, creating memory for the array, return NULL if error */
+/* get the value of the Hand, creating memory for the array, return NULL if error.
+ * array is formed by two integers. array[0] is max possible value (AS=11) or the only
+ * possible value if 0 or 12+ ases. array[1] is lowest possible value or -1 if there's only
+ * one possible value.*/
+
 int* hand_getValues(Hand*);
 
 /* divide the hand, returning and allocating memory for the new hand and modifying the original one 
