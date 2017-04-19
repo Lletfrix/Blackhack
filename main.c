2e0fc035a59434a1b240db80594794f41029f94c
lletfrix <lletfrix@gmail.com>
2017-04-19 16:55:26 +0200

    Error handling in main.c

--- main.c
+++ main.c
@@ -27,26 +27,34 @@ int main(int argc, char** argv) {
     }
     else numPartidas=atoi(argv[1]);
     Table *table = table_ini();
+
     if(!table)
     {
         fprintf(stderr, "main: error: failed to initialize table\n");
-        return 1;
+        return EXIT_FAILURE;
     }
 
     // initialize players
     // set the first player to use a random strategy
     table = table_addPlayer(table, player_ini(double_if_lose, play_random));
-
+    if(!table){
+        fprintf(stderr, "main: ERROR in table_addPlayer.\n");
+        return EXIT_FAILURE;
+    }
     for (int i = 1; i < NUMPLAYERS; i++) {
         // pasar funciones vacías para que de momento compile
         Player *p = player_ini(double_if_lose, play_like_crupier);
         if (!p) {
-            fprintf(stderr, "main: player_ini: error allocating memory for player %d\n", i);
+            fprintf(stderr, "main: player_ini: ERROR allocating memory for player %d\n", i);
             table_destroy(table);
-            return 2;
+            return EXIT_FAILURE;
         }
 
         table = table_addPlayer(table, p);
+        if(!table){
+            fprintf(stderr, "main: ERROR in table_addPlayer. Iteration: %d\n", i);
+            return EXIT_FAILURE;
+        }
     }
 
     // initialize crupier
@@ -57,7 +65,10 @@ int main(int argc, char** argv) {
         return 3;
     }
     table = table_setCrupier(table, crupier);
-
+    if(!table){
+        fprintf(stderr, "main: ERROR setting Crupier.\n");
+        return EXIT_FAILURE;
+    }
     // initialize deck, seeding with seed if provided
     unsigned seed;
     if (argc == 3) {
@@ -74,7 +85,10 @@ int main(int argc, char** argv) {
         return 4;
     }
     table = table_setDeck(table, deck);
-
+    if(!table){
+        fprintf(stderr, "Error setting Deck.\n");
+        return EXIT_FAILURE;
+    }
     // play a hundred times
     for (int k = 0; k < numPartidas; k++) {
 
@@ -132,5 +146,6 @@ table_printPlayersMoney(stdout, table);
 fprintf(stdout, "\n");
 table_printPlayersStreakData(stdout, table);
 
+table_destroy(table);
 return EXIT_SUCCESS;
 }
