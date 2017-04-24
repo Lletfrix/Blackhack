0ee068952f25a9d2c85ef3f315a8d79679d5d414
baque <baquedano.miguel@gmail.com>
2017-04-24 12:41:42 +0200

    added functions in probability.h

--- probability.h
+++ probability.h
@@ -1,1 +1,23 @@
-#define JKL 8
+#include hand.h
+#include deck.h
+#include player.h
+#include crupier.h
+#include table.h
+
+/*
+ ********************************
+ Posibles funciones a implementar
+ ********************************
+*/
+
+//returns the probability of card
+double probability_card(Deck *, int rank);
+
+//returns the probability of all the cards
+double * probability_listofcards(Table *);
+
+//return -1, 0, +1 used to count cards
+int probability_countingHiLo(int rank);
+
+//return remaining numbers of ases
+int probability_remainingAses(Deck *);
