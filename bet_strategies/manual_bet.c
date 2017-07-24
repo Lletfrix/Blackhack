79fd7445887c71afdc97508b71e0b1e6a5152dc7
baque <baquedano.miguel@gmail.com>
2017-07-24 21:13:27 +0200

    added manual_bet.c

--- bet_strategies/manual_bet.c
+++ bet_strategies/manual_bet.c
@@ -1,2 +1,23 @@
 #include "../bet_strategies.h"
 #include "../macros.h"
+
+Player* manual_bet(Player *p, Table *t){
+  int cash;
+
+  if(!p || !t){
+    fprintf(stderr, "manual_bet: error arguments pointing NULL");
+    return NULL;
+  }
+  fprintf(stdout, "Tienes %d rublos\n", player_getCash(p));
+  fprintf(stdout, "¿Cuantos rublos quieres apostar?\n");
+  fscanf(stdout, "%d", &cash);
+
+  p=player_removeCash(p, cash);
+  p=player_setLastBet(p, cash);
+  hand_setCurrentBet(player_getHand(p, 0), cash);
+  if(!p){
+    fprintf(stderr,"manual_bet: Error removing cash from player.\n");
+    return NULL;
+  }
+  return p;
+}
