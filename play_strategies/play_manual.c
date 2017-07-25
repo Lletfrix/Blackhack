728454ab45a61d06471b5e8ae3f91676d8cbaf6a
baque <baquedano.miguel@gmail.com>
2017-07-26 01:24:23 +0200

    manual_mode and play_manual

--- play_strategies/play_manual.c
+++ play_strategies/play_manual.c
@@ -5,7 +5,8 @@ Player *play_manual(Player *p, Table *t){
   Deck *deck=NULL;
   Crupier *crupier=NULL;
   Hand *playerHand=NULL, *crupierHand=NULL;
-  int *playerHand_values=NULL, *crupierHand_values=NULL;
+  int *playerHand_values=NULL, *crupierHand_values=NULL, rank;
+  char option;
 
   deck=table_getDeck(t);
   crupier=table_getCrupier(t);
@@ -19,25 +20,56 @@ Player *play_manual(Player *p, Table *t){
   printf("Tu mano actual:\t%d %d\n", playerHand_values[0], playerHand_values[1]);
   printf("Mano del crupier:%d %d\n", crupierHand_values[0], crupierHand_values[1]);
 
-  /*
-  printf("Lista de probabilidad:\n");
-  printf("\tAS\t%lf\t%lf%%\n", listofprob[0], listofprob[0]*100);
-  printf("\t2\t%lf\t%lf%%\n", listofprob[1],  listofprob[1]*100);
-  printf("\t3\t%lf\t%lf%%\n", listofprob[2],  listofprob[2]*100);
-  printf("\t4\t%lf\t%lf%%\n", listofprob[3],  listofprob[3]*100);
-  printf("\t5\t%lf\t%lf%%\n", listofprob[4],  listofprob[4]*100);
-  printf("\t6\t%lf\t%lf%%\n", listofprob[5],  listofprob[5]*100);
-  printf("\t7\t%lf\t%lf%%\n", listofprob[6],  listofprob[6]*100);
-  printf("\t8\t%lf\t%lf%%\n", listofprob[7],  listofprob[7]*100);
-  printf("\t9\t%lf\t%lf%%\n", listofprob[8],  listofprob[8]*100);
-  printf("\tT\t%lf\t%lf%%\n", listofprob[9],  listofprob[9]*100);
-  */
-
   printf("\tAS\t%lf\t%lf%%\n", listofprob[0], listofprob[0]*100);
   for(int i=1;i<9;i++){
     printf("\t%d\t%lf\t%lf%%\n", i+1,listofprob[i],  listofprob[i]*100);
   }
   printf("\tT\t%lf\t%lf%%\n", listofprob[9],  listofprob[9]*100);
+
+  if(playerHand_values[0]==21){
+      player_addCash(p, 2.5*hand_getCurrentBet(playerHand));
+      return p;
+  }
+
+  printf("¿Que quieres hacer?:\n");
+  printf("\t(H)it\n\t(S)tand\n\t(D)ouble\n");
+  scanf("\n%c", &option);
+
+  if(option=='s' || option=='S'){
+    return p;
+  }
+  else if(option=='d' || option=='D'){
+    fprintf(stdout, "¿Que carta(rank) te ha salido?\n");
+    fscanf(stdin, "%d", &rank);
+    hand_insertCard(playerHand, rank);
+    deck_removeCard(deck, rank);
+    return p;
+  }
+  else{
+    while(option=='h'||option=='H'){
+      fprintf(stdout, "¿Que carta(rank) te ha salido?\n");
+      fscanf(stdin, "%d", &rank);
+      hand_insertCard(playerHand, rank);
+      deck_removeCard(deck, rank);
+
+      playerHand_values=hand_getValues(playerHand);
+
+      listofprob=probability_listofcards(deck);
+      printf("Tu mano actual:\t%d %d\n", playerHand_values[0], playerHand_values[1]);
+      printf("Mano del crupier:%d %d\n", crupierHand_values[0], crupierHand_values[1]);
+
+      printf("\tAS\t%lf\t%lf%%\n", listofprob[0], listofprob[0]*100);
+      for(int i=1;i<9;i++){
+        printf("\t%d\t%lf\t%lf%%\n", i+1,listofprob[i],  listofprob[i]*100);
+      }
+      printf("\tT\t%lf\t%lf%%\n", listofprob[9],  listofprob[9]*100);
+
+      printf("¿Que quieres hacer?:\n");
+      printf("\t(H)it\n\t(S)tand\n");
+      scanf("\n%c", &option);
+    }
+    return p;
+  }
 
   return p;
 }
