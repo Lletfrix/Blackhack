8408ec99c51a5082cfd994be6abc34097cc7c498
lletfrix <lletfrix@gmail.com>
2017-07-28 03:21:53 +0200

    Added new matrix, implemented splitting

--- play_strategies/play_basic_17S_DAS_matrix.c
+++ play_strategies/play_basic_17S_DAS_matrix.c
@@ -1,18 +1,18 @@
 #include "../play_strategies.h"
 
-char recommendation_17S_DAS(int player_hand, int crupier_hand) {
+char recommendation_17S_DAS(int player_hand, int crupier_hand, Hand* h) {
 
     /*
      * x is for hit, D is for double-down, S is for split, a is for early surrender,
      * - is for stand.
      *
-     *   To acces this matrix:
+     *   To access this matrix:
      *   number_strategy[PLAYER_CONVERT(player_hand)][CRUPIER_CONVERT(crupier_hand)]
      */
 
-    char number_strategy[17][10] = {
+    char number_strategy[34][10] = {
     //    2    3    4    5    6    7    8    9    10   A
-        {'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'a'}, // 5
+        {'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'a'}, // 5 #0
         {'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'a'}, // 6
         {'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'a'}, // 7
         {'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x'}, // 8
@@ -22,18 +22,53 @@ char recommendation_17S_DAS(int player_hand, int crupier_hand) {
         {'x', 'x', '-', '-', '-', 'x', 'x', 'x', 'x', 'a'}, // 12
         {'-', '-', '-', '-', '-', 'x', 'x', 'x', 'x', 'a'}, // 13
         {'-', '-', '-', '-', '-', 'x', 'x', 'x', 'a', 'a'}, // 14
-        {'-', '-', '-', '-', '-', 'x', 'x', 'x', 'a', 'a'}, // 15
+        {'-', '-', '-', '-', '-', 'x', 'x', 'x', 'a', 'a'}, // 15 #10
         {'-', '-', '-', '-', '-', 'x', 'x', 'a', 'a', 'a'}, // 16
         {'-', '-', '-', '-', '-', '-', '-', '-', '-', '-'}, // 17
         {'-', '-', '-', '-', '-', '-', '-', '-', '-', 'a'}, // 18
         {'-', '-', '-', '-', '-', '-', '-', '-', '-', 'a'}, // 19
         {'-', '-', '-', '-', '-', '-', '-', '-', '-', 'a'}, // 20
         {'-', '-', '-', '-', '-', '-', '-', '-', '-', 'a'}, // 21
+        {'x', 'x', 'x', 'D', 'D', 'x', 'x', 'x', 'x', 'x'}, // A-2
+        {'x', 'x', 'x', 'D', 'D', 'x', 'x', 'x', 'x', 'x'}, // A-3
+        {'x', 'x', 'D', 'D', 'D', 'x', 'x', 'x', 'x', 'x'}, // A-4
+        {'x', 'x', 'D', 'D', 'D', 'x', 'x', 'x', 'x', 'x'}, // A-5 #20
+        {'x', 'D', 'D', 'D', 'D', 'x', 'x', 'x', 'x', 'x'}, // A-6
+        {'-', 'D', 'D', 'D', 'D', '-', '-', 'x', 'x', 'x'}, // A-7
+        {'-', '-', '-', '-', '-', '-', '-', '-', '-', '-'}, // A-8,9,T
+        {'s', 's', 's', 's', 's', 's', 's', 's', 's', 's'}, // A-A
+        {'s', 's', 's', 's', 's', 's', 'x', 'x', 'x', 'x'}, // 2-2
+        {'s', 's', 's', 's', 's', 's', 'x', 'x', 'x', 'a'}, // 3-3
+        {'x', 'x', 'x', 's', 's', 'x', 'x', 'x', 'x', 'x'}, // 4-4
+        {'D', 'D', 'D', 'D', 'D', 'D', 'D', 'D', 'x', 'x'}, // 5-5
+        {'s', 's', 's', 's', 's', 's', 'x', 'x', 'x', 'a'}, // 6-6
+        {'s', 's', 's', 's', 's', 's', 'x', 'x', 'a', 'a'}, // 7-7 #30
+        {'s', 's', 's', 's', 's', 's', 's', 's', 's', 's'}, // 8-8
+        {'s', 's', 's', 's', 's', '-', 's', 's', '-', '-'}, // 9-9
+        {'-', '-', '-', '-', '-', '-', '-', '-', '-', '-'}, // T-T
     };
 
-    if (player_hand > 21 || player_hand < 5) {
+    int player_combination[10][10] = {
+    //    2  3  4  5  6  7  8  9  T  A
+        {25, 0, 1, 2, 3, 4, 5, 6, 7, 17}, // 2
+        { 0,26, 2, 3, 4, 5, 6, 7, 8, 18}, // 3
+        { 1, 2,27, 4, 5, 6, 7, 8, 9, 19}, // 4
+        { 2, 3, 4,28, 6, 7, 8, 9,10, 20}, // 5
+        { 3, 4, 5, 6,29, 8, 9,10,11, 21}, // 6
+        { 4, 5, 6, 7, 8,30,10,11,12, 22}, // 7
+        { 5, 6, 7, 8, 9,10,31,12,13, 23}, // 8
+        { 6, 7, 8, 9,10,11,12,32,14, 23}, // 9
+        { 7, 8, 9,10,11,12,13,14,33, 23}, // T
+        {17,18,19,20,21,22,23,23,23, 24}, // A
+    };
+
+    if (player_hand > 21 || player_hand < 5 || hand_getNumCards(h) < 2) {
         fprintf(stderr, "strategy can't recommend anything for that value (%d)\n", player_hand);
         return 'E';
+    }
+
+    if (hand_getNumCards(h) == 2){
+        
     }
 
     return number_strategy[player_hand - 5][crupier_hand - 2];
@@ -41,7 +76,7 @@ char recommendation_17S_DAS(int player_hand, int crupier_hand) {
 
 Player *play_basic_17S_DAS_matrix(Player* p, Table* t){
 
-    int *player_hand, *crupier_hand, player_value;
+    int *player_hand, *crupier_hand, player_value, active_hand=0, numHands=1, index=-1;
     char decision;
     Deck *d;
     Hand *current_hand;
@@ -59,8 +94,8 @@ Player *play_basic_17S_DAS_matrix(Player* p, Table* t){
         return NULL;
     }
 
-    while (1) {
-        player_hand = hand_getValues(player_getHand(p, 0));
+    while (active_hand < numHands) {
+        player_hand = hand_getValues(player_getHand(p, active_hand));
         if (!player_hand) {
             fprintf(stderr, "play_strategy: handValues pointing NULL\n");
             return NULL;
@@ -71,31 +106,42 @@ Player *play_basic_17S_DAS_matrix(Player* p, Table* t){
 
         // Blackjack! Pay immediately;
         if (player_value == 21){
-            player_addCash(p, 2.5*hand_getCurrentBet(player_getHand(p, 0)));
+            player_addCash(p, 2.5*hand_getCurrentBet(player_getHand(p, active_hand)));
             return p;
         }
 
-        decision = recommendation_17S_DAS(player_value, crupier_hand[0]);
+        decision = recommendation_17S_DAS(player_value, crupier_hand[0], player_getHand(p, active_hand));
+        /*
+        *
+        * Change decision depending on the situation. To be implemented.
+        *
+        */
         printf("%d vs %d: player choses to %c\n", player_value, crupier_hand[0], decision);
         switch(decision) {
             // hit, ask for another card
             case 'x':
-                p = player_addCardToHand(p, 0, deck_draw(d));
+                p = player_addCardToHand(p, active_hand, deck_draw(d));
                 break;
-            case 's':
-                printf("splitting is not implemented at the moment\n");
-                return p;
+            case 's':  //split
+                index = player_splitHand(p, active_hand);
+                p = player_addCardToHand(p, active_hand, deck_draw(d));
+                p = player_addCardToHand(p, index, deck_draw(d));
+                int current_bet = hand_getCurrentBet(player_getHand(p, active_hand));
+                p = player_removeCash(p, current_bet);
+                hand_setCurrentBet(player_getHand(p, index), current_bet);
+                numHands++;
+                break;
             case 'D': // double
-                current_hand = player_getHand(p, 0);
-                p = player_addCardToHand(p, 0, deck_draw(d));
-                int current_bet = hand_getCurrentBet(current_hand);
+                current_hand = player_getHand(p, active_hand);
+                p = player_addCardToHand(p, active_hand, deck_draw(d));
+                current_bet = hand_getCurrentBet(current_hand);
                 p = player_removeCash(p, current_bet);
                 hand_setCurrentBet(current_hand, current_bet * 2);
-                return p;
+                break;
             default: // stay
-                return p;
+                break;
         }
-
+        active_hand++;
 
     }
 
